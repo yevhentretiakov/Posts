49bcadce84bb537d0fd3c9ae4d5e26e833f00dd6
Yevhen Tretiakov <yevhen.tretiakov@gmail.com>
2022-09-02 11:12:26 +0300

    Prepared the project for work

---
+++ Posts/Posts/Application/SceneDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  SceneDelegate.swift
+//  Posts
+//
+//  Created by Yevhen Tretiakov on 02.09.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        setupRootView(scene: scene)
+    }
+    
+    private func setupRootView(scene: UIScene) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = FeedViewController()
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+}
---
+++ Posts/Posts/Modules/Feed/View/FeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Posts
+//
+//  Created by Yevhen Tretiakov on 02.09.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    // MARK: - Methods
+}
